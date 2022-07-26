821e30880a0cd224f970ab63a6c73da869132a05
miki <mislav.pozek@gmail.com>
2022-07-26 13:18:23 +0200

    move db pwds to .env

--- db/seed/init-mongo.js
+++ db/seed/init-mongo.js
@@ -1,6 +1,6 @@
 /** All interactive Mongo shell commands can be run from here - it's a lot easier in JS when you need loops etc */
 
-db.auth("root", "example")
+db.auth("root", pass);
 db = db.getSiblingDB('team-finder')
 
 // Tear down existing auth collection context - sometimes we lose the index otherwise (for some reason)
